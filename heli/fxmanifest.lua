01b579ee684e5607f4a40c0e60fc6ee29ad1ed12
C++ <103396716+takennnncs@users.noreply.github.com>
2024-06-17 21:05:02 +0300

    Update fxmanifest.lua

--- heli/fxmanifest.lua
+++ heli/fxmanifest.lua
@@ -1,9 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 
--- Manifest
-author 'mraes | https://forum.cfx.re/u/mraes/summary'
-description 'FiveM LSPD Heli Cam by mraes'
+author 'takenncs'
+description 'LSPD Helicam'
 
 client_script 'heli_client.lua'
 server_script 'heli_server.lua'
